3e7167c89a13cbb9997a3574175017e380171166
Tony Rizzotto <tony@tonyrizzotto.com>
2023-04-26 22:08:41 -0400

    chore: fix width app wrapper

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import { Box, Button } from '@mui/material';
 import PageWrapper from '../../components/PageWrapper';
 
 export default function Blog() {
+  const navigate = useNavigate();
   return (
-    <PageWrapper>My Blog</PageWrapper>
+    <PageWrapper>
+      <Box>
+        I have some blog posts, son!
+      </Box>
+      <Button onClick={() => navigate('/blog/2023-04-22')}>My First Post</Button>
+    </PageWrapper>
   );
 }
--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,9 +1,18 @@
 import { Box } from '@mui/material';
-
+import { useTheme } from '@mui/material/node/styles';
 // eslint-disable-next-line react/prop-types
 export default function AppContainer({ children }) {
+  const { breakpoints } = useTheme();
   return (
-    <Box sx={{ margin: '0 5%' }}>
+    <Box
+      sx={{
+        margin: '0 5%',
+        [breakpoints.up('lg')]: {
+          margin: '0 auto',
+          maxWidth: '1100px',
+        },
+      }}
+    >
       {children}
     </Box>
   );
