1c0fce86a92e7c46ae3e0b8feab427166ac0dec0
Tony Rizzotto <tony@tonyrizzotto.com>
2023-05-08 21:53:51 -0400

    feat(optimization): enable text compression

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 // eslint-disable-next-line import/no-unresolved
 import { cjsInterop } from 'vite-plugin-cjs-interop';
 
@@ -10,6 +11,7 @@ export default defineConfig({
   root: join(dirname(fileURLToPath(new URL(import.meta.url))), 'src'),
   plugins: [
     react({ jsxRuntime: '@emotion/react' }),
+    viteCompression(),
     cjsInterop({
       dependencies: [
         '@emotion/cache',
